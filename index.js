761ffb75b3f9a0a9783862fa90c26995becda3df
Tre Ammatuna <tre.ammatuna@leafly.com>
2020-02-20 23:12:55 -0800

    chore(cleanup): cleanup logs and update README

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-// most @actions toolkit packages have async methods
 async function run() {
   const fromBranch = core.getInput("FROM_BRANCH", { required: true });
   const toBranch = core.getInput("TO_BRANCH", { required: true });
   const githubToken = core.getInput("GITHUB_TOKEN", { required: true });
 
   try {
-    console.log(`Making a PR to ${toBranch} from ${fromBranch}`);
+    console.log(`Making a pull request to ${toBranch} from ${fromBranch}.`);
 
     const actionContext = github.context;
-    console.log("💣🔥>>>>>>>: run -> actionContext", actionContext);
 
     const octokit = new github.GitHub(githubToken);
 
@@ -23,7 +21,9 @@ async function run() {
       base: toBranch
     });
 
-    console.log("💣🔥>>>>>>>: run -> pullRequest", pullRequest);
+    console.log(
+      `Pull request successful! You can see it here: ${pullRequest.url}.`
+    );
   } catch (error) {
     core.setFailed(error.message);
   }
