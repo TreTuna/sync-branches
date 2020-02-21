32a596e2546d5a1c2817ac1bf1e319697fe1cb74
Tre Ammatuna <tre.ammatuna@leafly.com>
2020-02-20 22:49:31 -0800

    feat(action): don't destructure off of context

--- dist/index.js
+++ dist/index.js
@@ -1987,24 +1987,21 @@ async function run() {
   const toBranch = core.getInput("TO_BRANCH");
   try {
     console.log(`Making a PR to ${toBranch} from ${fromBranch}`);
+
     const actionContext = JSON.stringify(github.context, undefined, 2);
-    const {
-      payload: { repository }
-    } = actionContext;
+    console.log("💣🔥>>>>>>>: run -> actionContext", actionContext);
 
     const githubToken = core.getInput("GITHUB_TOKEN");
-
     const octokit = new github.GitHub(githubToken);
 
     const { data: pullRequest } = await octokit.pulls.create({
-      owner: repository.owner,
-      repo: repository.name,
+      owner: actionContext.payload.repository.owner.login,
+      repo: actionContext.payload.repository.name,
       title: `sync: ${fromBranch} to ${toBranch}`,
       head: fromBranch,
       base: toBranch
     });
-
-    console.log(pullRequest);
+    console.log("💣🔥>>>>>>>: run -> pullRequest", pullRequest);
   } catch (error) {
     core.setFailed(error.message);
   }
